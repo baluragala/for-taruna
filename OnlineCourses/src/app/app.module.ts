70f75f6f7f375a5cfbb51c1c315c6cc482d5a51c
Bala Krishna Ragala <baluragala@sparity.com>
2017-06-01 09:54:04 +0530

    Day 9,ES6 modules, destructuring,  angular2 components

---
+++ OnlineCourses/src/app/app.module.ts
@@ -0,0 +1,24 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { HttpModule } from '@angular/http';
+
+import { AppComponent } from './app.component';
+import { HelloComponent } from './hello.component';
+import { GoodByeComponent } from './good-bye/good-bye.component'
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    HelloComponent,
+    GoodByeComponent
+  ],
+  imports: [
+    BrowserModule,
+    FormsModule,
+    HttpModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
